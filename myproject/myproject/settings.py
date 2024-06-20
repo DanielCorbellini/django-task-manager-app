ceef3775258d6be1d4b814542626a1673b63a3bf
Daniel Augusto Corbellini <daniel.corbellini@pml.lajeado.rs.gov.br>
2024-06-20 15:53:44 -0300

    Finishing the project

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'todolist'
+LOGIN_URL = 'login'
--- myproject/user_app/urls.py
+++ myproject/user_app/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
-
 ]
